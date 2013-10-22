8b9578db456b884056c4cf18e18b5ba909581ce1
Mel09 <melanie.roy09@gmail.com>
2013-10-22 17:46:13 +0200

    nouvelles modifs !

--- header.php
+++ header.php
@@ -2,19 +2,21 @@
 	<div id="divLogo"><img src="graphics/logo.png" /></div>
 	<nav>
 		<ul id="mainmenu">
-			<li class="first"><a href="">L'histoire</a>
+        	<li><a href="index.php?page=1">Lire la nouvelle</a></li>
+			<li><a href="">L'histoire</a>
 				<ul class="sousmenu">
-					<li><a href="index.php?page=11">L'univers</a></li>
+					<li><a href="index.php?page=10">Synopsis</a></li>
+                    <li><a href="index.php?page=11">L'univers</a></li>
 					<li><a href="index.php?page=12">Les personnages</a></li>
 					<li><a href="index.php?page=13">Le parcours initiatique</a></li>
 				</ul>
 			</li>
-			<li class="second"><a href="index.php?page=20">Et après</a></li>
-			<li class="third"><a href="index.php?page=30">Les auteurs</a></li>
-			<li class="fourth"><a href="index.php?page=40">Bonus</a></li>
+			<li><a href="index.php?page=20">Et après</a></li>
+			<li><a href="index.php?page=30">Les auteurs</a></li>
+			<li><a href="index.php?page=40">Bonus</a></li>
 		</ul>
 	</nav>
 	<div>
-		BORDEL
+    
 	</div>
 </header>
