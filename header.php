540aa7c8848c151b9cf5b63febe4ae9d2915068a
loicsport23 <loicsport23@gmail.com>
2013-10-24 09:24:15 +0200

    Petite bordure + background sur menu

--- header.php
+++ header.php
@@ -1,26 +1,28 @@
 <header>
-	<!-- PARTIE LOGO -->
-	<div id="divLogo"><a href="index.php"><img src="graphics/logo.png" alt="Culin'R" /></a></div>
-	<!-- MENU DE NAVIGATION -->
-	<nav>
-		<ul id="mainmenu">
-        	<li><a href="index.php?page=lire">Lire la nouvelle</a></li>
-			<li onclick="expand('smHIS');"><a href="#">L'histoire</a>
-				<ul class="sousmenu" id="smHIS">
-					<li><a href="index.php?page=synopsis">Synopsis</a></li>
-                    <li><a href="index.php?page=univers">L'univers</a></li>
-					<li><a href="index.php?page=personnages">Les personnages</a></li>
-					<li><a href="index.php?page=parcours-initiatique">Le parcours initiatique</a></li>
-				</ul>
-			</li>
-			<li><a href="index.php?page=et-apres">Et après</a></li>
-			<li><a href="index.php?page=auteurs">Les auteurs</a></li>
-            <li onclick="expand('smBON');"><a href="#">Bonus</a>
-				<ul class="sousmenu" id="smBON">
-					<li><a href="index.php?page=inspirations">Inspirations</a></li>
-                    <li><a href="index.php?page=recettes">Recettes du futur</a></li>
-				</ul>
-			</li>
-		</ul>
-	</nav>
+	<div>
+		<!-- PARTIE LOGO -->
+		<div id="divLogo"><a href="index.php"><img src="graphics/logo.png" alt="Culin'R" /></a></div>
+		<!-- MENU DE NAVIGATION -->
+		<nav>
+			<ul id="mainmenu">
+	        	<li><a href="index.php?page=lire">Lire la nouvelle</a></li>
+				<li onclick="expand('smHIS');"><a href="#">L'histoire</a>
+					<ul class="sousmenu" id="smHIS">
+						<li><a href="index.php?page=synopsis">Synopsis</a></li>
+	                    <li><a href="index.php?page=univers">L'univers</a></li>
+						<li><a href="index.php?page=personnages">Les personnages</a></li>
+						<li><a href="index.php?page=parcours-initiatique">Le parcours initiatique</a></li>
+					</ul>
+				</li>
+				<li><a href="index.php?page=et-apres">Et après</a></li>
+				<li><a href="index.php?page=auteurs">Les auteurs</a></li>
+	            <li onclick="expand('smBON');"><a href="#">Bonus</a>
+					<ul class="sousmenu" id="smBON">
+						<li><a href="index.php?page=inspirations">Inspirations</a></li>
+	                    <li><a href="index.php?page=recettes">Recettes du futur</a></li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+	</div>
 </header>
