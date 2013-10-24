661cecd8110701a5770863ed13dd6533f83d39ed
loicsport23 <loicsport23@gmail.com>
2013-10-24 12:07:37 +0200

    Changement du css

--- scripts/scripts.php
+++ scripts/scripts.php
@@ -4,6 +4,14 @@
 	 *  nomSM représente l'ID du block à afficher
 	 */ 
 	function expand(nomSM) {
+		// Un clic a eu lieu sur un élément, on referme donc tous les sous-menus
+		var sousmenus = document.getElementsByClassName("sousmenu");
+		// On parcours l'élément avec une boucle
+		// La boucle for a 3 paramètres : l'initialisation, la limite pour l'arrêt, l'incrémentation
+		for(var i = 0; i < sousmenus.length; i++) {
+			sousmenus[i].style.display = "none";
+		}
+
 		// On récupère l'élément via son ID
 		sousliste = document.getElementById(nomSM);
 		// On change sa propriété display: none en display:block
