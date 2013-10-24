a5a6995b94d3c7f48d709fa863102028d5ac2f18
loicsport23 <loicsport23@gmail.com>
2013-10-24 16:46:54 +0200

    Tri dans le CSS

--- header.php
+++ header.php
@@ -3,6 +3,8 @@
 		<!-- PARTIE LOGO -->
 		<div id="divLogo"><a href="index.php"><img src="graphics/logo.png" alt="Culin'R" /></a></div>
 		<!-- MENU DE NAVIGATION -->
+		<div id="blocNavigationPourMobileIn" onclick="expandNav();"><a href="#">NAVIGATION IN</a></div>
+		<div id="blocNavigationPourMobileOut" onclick="unexpandNav();"><a href="#">NAVIGATION OUT</a></div>
 		<nav>
 			<ul id="mainmenu">
 	        	<li><a href="index.php?page=lire">Lire la nouvelle</a></li>
--- scripts/scripts.php
+++ scripts/scripts.php
@@ -1,9 +1,6 @@
 <script type="text/javascript">
-	/**
-	 *  Fonction qui permet d'afficher un block en display:none;
-	 *  nomSM représente l'ID du block à afficher
-	 */ 
-	function expand(nomSM) {
+
+	function hideSM() {
 		// Un clic a eu lieu sur un élément, on referme donc tous les sous-menus
 		var sousmenus = document.getElementsByClassName("sousmenu");
 		// On parcours l'élément avec une boucle
@@ -11,10 +8,38 @@
 		for(var i = 0; i < sousmenus.length; i++) {
 			sousmenus[i].style.display = "none";
 		}
+	};
+
+	/**
+	 *  Fonction qui permet d'afficher un block en display:none;
+	 *  nomSM représente l'ID du block à afficher
+	 */ 
+	function expand(nomSM) {
+		alert("function expand");
+		hideSM();
 
 		// On récupère l'élément via son ID
 		sousliste = document.getElementById(nomSM);
 		// On change sa propriété display: none en display:block
 		sousliste.style.display = "block";
+	};
+
+	function expandNav() {
+		var mainmenu = document.getElementsByTagName("nav");
+		var blockIn = document.getElementById("blocNavigationPourMobileIn");
+		var blockOut = document.getElementById("blocNavigationPourMobileOut");
+
+		mainmenu[0].style.display = "block";
+		blockIn.style.display = "none";
+		blockOut.style.display = "block";
+	};
+	function unexpandNav() {
+		var mainmenu = document.getElementsByTagName("nav");
+		var blockIn = document.getElementById("blocNavigationPourMobileIn");
+		var blockOut = document.getElementById("blocNavigationPourMobileOut");
+
+		mainmenu[0].style.display = "none";
+		blockIn.style.display = "block";
+		blockOut.style.display = "none";
 	};
 </script>
