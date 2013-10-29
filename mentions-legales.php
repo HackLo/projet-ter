4ad50d1af6b5e0e01b5f310df8470b9a7cbbc44f
Mel09 <melanie.roy09@gmail.com>
2013-10-29 14:54:09 +0100

    Contenu page mentions légales

--- mentions-legales.php
+++ mentions-legales.php
@@ -1,4 +1,26 @@
 <div id="wrapper">
     <div id="titrepage">Mentions légales</div>
-	<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+	    
+    <p class="soustitre">A PROPOS DU SITE CULIN'R</p>
+		<p>Réalisation du site dans le cadre d'un projet TER. Le site à été réalisé par un groupe d'étudiants du Master Web Éditorial, promotion 2015 :</p>
+			<ul>
+        		<li>Loïc Barouty</li>
+                <li>Vasylyna Golovnya</li>
+                <li>Lucas Leroux</li>
+                <li>Mélanie Roy</li>
+                <li>Romain Ouvrard</li>
+                <li>Esther Wiest</li>
+			</ul>
+
+	<p class="soustitre">RESPECT DE LA VIE PRIVÉE ET DES DONNÉES PERSONNELLES</p>
+    	<p>Les informations obtenues par le biais de formulaires ou cookies sur le site sont destinées à des fins de communications et de contacts. Elles ne seront en aucun cas mises à disposition ou vendues à des tierces parties. Aucun traitement de données n'est effectué.</p>
+
+	<p class="soustitre">DROITS D'AUTEUR</p>
+		<p>Toute reproduction ou représentation, intégrale ou partielle, du présent site internet, faîte sans le consentement des auteurs, serait illicite et constituerait un délit de contrefaçon (article L.335-2 du Code de la Propriété Intellectuelle), engageant la responsabilité civile ou pénale de l'auteur de la contrefaçon.</p>
+		<p>Les textes et informations proposés sur ce présent site internet sont la propriété de leurs auteurs. Les images et illustrations utilisées sont réalisées par les auteurs du site.</p>
+		<p>Il est interdit de reproduire, représenter, transférer, distribuer, ou d'enregistrer tout ou partie de ces éléments, sous quelque forme que ce soit, sans l'accord préalable et écrit des auteurs du site.</p>
+
+	<p class="soustitre">LIENS</p>
+		<p>Les sites reliés directement ou indirectement au présent site ne sont pas sous son contrôle. En conséquence, les auteurs du site n'assument aucune responsabilité quant aux informations publiées sur ces sites. Les liens avec des sites extérieurs ne sont fournis qu'à titre de commodité et n'impliquent aucune caution quant à leur contenu.</p>
+   
 </div>
