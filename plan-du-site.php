774362055a61080b1be69bba8f6736c5548c530a
Mel09 <melanie.roy09@gmail.com>
2013-11-15 23:45:25 +0100

    modif plan du site

--- plan-du-site.php
+++ plan-du-site.php
@@ -11,9 +11,7 @@
             <blockquote><li class="niveaudeux"><a href="index.php?page=parcours-initiatique">Le parcours initiatique</a></li></blockquote>
         <li><a href="index.php?page=et-apres">Et après</a></li>
         <li><a href="index.php?page=auteurs">Les auteurs</a></li>
-        <li>Bonus :</li>
-            <blockquote><li class="niveaudeux"><a href="index.php?page=inspirations">Inspirations</a></li></blockquote>
-            <blockquote><li class="niveaudeux"><a href="index.php?page=recettes">Recettes du futur</a></li></blockquote>
+        <li><a href="index.php?page=inspirations">Inspirations</a></li><br />
         <li><a href="index.php?page=plan">Plan du site</a></li>
         <li><a href="index.php?page=mentions">Mentions légales</a></li>
 	</ul>
