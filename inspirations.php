901f2d4f8992a8ffe407cd39228d7d2382967388
Mel09 <melanie.roy09@gmail.com>
2013-11-15 23:42:27 +0100

    page inspirations

--- inspirations.php
+++ inspirations.php
@@ -1,4 +1,45 @@
 <div id="wrapper">
-    <div id="titrepage">Inspirations</div>
-	<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+
+	<div id="titrepage">Inspirations</div>
+  
+	<div class="blocleft">
+		<p class="soustitre">INCEPTION</p>
+		<p>Film de Christopher Nolan</p>
+		<img src="graphics/inspiration-inception.jpg" class="imagecontenu" width="190" alt="Inception" />
+	</div>
+	<div class="blocright">
+		<p>Dans un futur proche, l'armée américaine a développé le "rêve partagé", une méthode permettant d'influencer l'inconscient d'une victime pendant qu'elle rêve. Ainsi, on découvre la ville des "limbes" dont nous nous sommes inspirés pour le monde à l'extérieur du dôme. Les limbes, sorte de dimension de rêve parallèle, sont l'un des éléments les plus abstraits et mystérieux du film.</p>
+	</div>
+
+
+	<div class="blocleft">
+		<p class="soustitre">UNDER THE DOME</p>
+		<p>Série TV de Brian K. Vaughan,<br /> 
+        d'après le roman de Stephen King</p>
+		<img src="graphics/inspiration-underthedome.jpg" class="imagecontenu" width="190" alt="Inception" />
+	</div>
+	<div class="blocright">
+		<p>A Chester's Mill, les habitants se réveillent un matin coupés du monde et piégés dans la ville par un immense dôme transparent. Certains tenteront de tirer profit de cette situation inexpliquée afin de prendre le pouvoir. Mais une résistance va s'organiser  pour empêcher ces personnes de parvenir à leur fin. Ce dôme est à l'origine même de l'idée des deux univers de cette nouvelle.</p>
+	</div>
+
+
+	<div class="blocleft">
+		<p class="soustitre">LIVING TOMORROW</p>
+		<p>Projet de Michael van Oosten</p>
+		<img src="graphics/inspiration-livingtomorrow.jpg" class="imagecontenu" width="190" alt="Inception" />
+	</div>
+	<div class="blocright">
+		<p>Living Tomorrow est un lieu de rencontres pour les entreprises novatrices, leur permettant de faire connaître aux visiteurs des produits et services qui pourraient améliorer la qualité de vie et de travail, dans un futur proche. Ce projet a inspiré notre vision de la maison d'Ed Gaïa, notamment la maison du futur d'Amsterdam (pour l'extérieur) et celle de Vilvorde (pour l'intérieur et la cuisine).</p>
+	</div>
+
+
+	<div class="blocleft">
+		<p class="soustitre">GRAND LIVRE DE CUISINE</p>
+		<p>de Joël Robuchon</p>
+		<img src="graphics/inspiration-grandlivredecuisine.jpg" class="imagecontenu" width="190" alt="Inception" />
+	</div>
+	<div class="blocright">
+		<p>Au travers de cet ouvrage, Joël Robuchon s'adresse aux cuisiniers et amateurs passionnés, et démontre son amour du produit, son envie de le sublime et son désir de partager le plus largement possible le fruit de ses créations. Cette nouvelle fait référence à ce livre d'art culinaire qui reflète les maîtres mots de la cuisine du chef le plus étoilé au monde: excellence et tradition.</p>
+	</div>
+
 </div>
