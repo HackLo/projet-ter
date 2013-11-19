98c8bbec1935ee45530125636978462c4d3ca56a
Mel09 <melanie.roy09@gmail.com>
2013-11-19 14:13:21 +0100

    alt page inspiration

--- inspirations.php
+++ inspirations.php
@@ -16,7 +16,7 @@
 		<p class="soustitre">UNDER THE DOME</p>
 		<p>Série TV de Brian K. Vaughan,<br /> 
         d'après le roman de Stephen King</p>
-		<img src="graphics/inspiration-underthedome.jpg" class="imagecontenu" width="190" alt="Inception" />
+		<img src="graphics/inspiration-underthedome.jpg" class="imagecontenu" width="190" alt="Under the Dome" />
 	</div>
 	<div class="blocright">
 		<p>A Chester's Mill, les habitants se réveillent un matin coupés du monde et piégés dans la ville par un immense dôme transparent. Certains tenteront de tirer profit de cette situation inexpliquée afin de prendre le pouvoir. Mais une résistance va s'organiser  pour empêcher ces personnes de parvenir à leur fin. Ce dôme est à l'origine même de l'idée des deux univers de cette nouvelle.</p>
@@ -26,7 +26,7 @@
 	<div class="blocleft">
 		<p class="soustitre">LIVING TOMORROW</p>
 		<p>Projet de Michael van Oosten</p>
-		<img src="graphics/inspiration-livingtomorrow.jpg" class="imagecontenu" width="190" alt="Inception" />
+		<img src="graphics/inspiration-livingtomorrow.jpg" class="imagecontenu" width="190" alt="Living Tomorrow" />
 	</div>
 	<div class="blocright">
 		<p>Living Tomorrow est un lieu de rencontres pour les entreprises novatrices, leur permettant de faire connaître aux visiteurs des produits et services qui pourraient améliorer la qualité de vie et de travail, dans un futur proche. Ce projet a inspiré notre vision de la maison d'Ed Gaïa, notamment la maison du futur d'Amsterdam (pour l'extérieur) et celle de Vilvorde (pour l'intérieur et la cuisine).</p>
@@ -36,7 +36,7 @@
 	<div class="blocleft">
 		<p class="soustitre">GRAND LIVRE DE CUISINE</p>
 		<p>de Joël Robuchon</p>
-		<img src="graphics/inspiration-grandlivredecuisine.jpg" class="imagecontenu" width="190" alt="Inception" />
+		<img src="graphics/inspiration-grandlivredecuisine.jpg" class="imagecontenu" width="190" alt="Geand livre de cuisine" />
 	</div>
 	<div class="blocright">
 		<p>Au travers de cet ouvrage, Joël Robuchon s'adresse aux cuisiniers et amateurs passionnés, et démontre son amour du produit, son envie de le sublime et son désir de partager le plus largement possible le fruit de ses créations. Cette nouvelle fait référence à ce livre d'art culinaire qui reflète les maîtres mots de la cuisine du chef le plus étoilé au monde: excellence et tradition.</p>
