4c48ee1e091130eeb4fdf28849e090d076384e86
Vasylyna <gvasylyna@gmail.com>
2013-11-19 14:30:09 +0100

    33

--- footer.php
+++ footer.php
@@ -1,5 +1,4 @@
 <footer>
-<<<<<<< HEAD
 	<div class="reseauxsociaux">
 		<a href=""><img src="graphics/facebook.png" alt="Page Facebook Culin'R" width="28" height="28" /></a>
 		<a href=""><img src="graphics/youtube.png" alt="Page Youtube Culin'R" width="28" height="28" /></a>
@@ -8,7 +7,4 @@
 		<p class="copyright">© Copyright 2013 - Master Web Éditorial</p>
 		<p class="copyright"><a href="index.php?page=plan">Plan du site</a> | <a href="index.php?page=mentions">Mentions légales</a></p>
 	</div>
-=======
-			
->>>>>>> parent of 61d97e3... salut
 </footer>
