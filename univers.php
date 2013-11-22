9c569db8941019a26b9d3723ce3e5d1c3ebeacd2
lucalerou <lucalerou@gmail.com>
2013-11-22 12:03:00 +0100

    Update univers.php

--- univers.php
+++ univers.php
@@ -1,18 +1,38 @@
 <div id="wrapper">
-    <div id="titrepage">L'univers</div>
+    <h1 id="titrepage">L'univers</h1>
 	
-    <p class="soustitre">Les deux mondes</p>
-	<p>Notre planète est un amoncellement de cendres sans âme dans lequel le vivant n'a plus vraiment son mot à dire. Seule espèce capable d'affronter les conséquences d'une explosion volcanique dévastatrice, l'Humain a choisi de profondément modifier son métabolisme pour amoindrir les dangers et douleurs imposés par cet environnement, qui est désormais son principal prédateur. Contraint de se prémunir pour survivre, il se retrouve esclave des technologies transhumanistes dont il rêvait autrefois et le plaisir simple de la nourriture a disparu depuis bien longtemps de son mode de vie.</p>
-	<p>Le minerais d'uranium est la principale source d'énergie de ce nouveau monde, et comme toute ressource naturelle, son exploitation est synonyme de son épuisement. L'ennemi commun a changé, et l'armée s'est adaptée : c'est elle qui explore les terres environnantes à la recherche de nouveaux gisements.</p>
-	<p>Loin de cette civilisation déshumanisée, l'éminent Général Raynor Hell découvrira une communauté vivant en autarcie qui s'était préparée à la catastrophe, offrant à ses sages habitants une solution viable pour leur survie sur le long terme. Le Dôme les protège, les alimente en énergie et leur offre un écosystème florissant que le reste du monde croyait disparu à jamais.</p><br />
+    <h2 class="soustitre">La planète Terre</h2>
+	
 
-	<p class="soustitre">La maison d'Ed Gaïa</p>
+<p>Notre planète est un amoncellement de cendres sans âme dans lequel le vivant n'a plus vraiment son mot à dire. Seule espèce capable d'affronter les conséquences d'une explosion volcanique dévastatrice, l'Humain a choisi de profondément modifier son métabolisme pour amoindrir les dangers et douleurs imposés par cet environnement, qui est désormais son 
+
+principal prédateur. Contraint de se prémunir pour survivre, il se retrouve esclave des technologies transhumanistes dont il rêvait autrefois et le plaisir simple de la 
+
+nourriture a disparu depuis bien longtemps de son mode de vie. Le minerais d'uranium est la principale source d'énergie de ce nouveau monde, et comme toute ressource naturelle, 
+
+son exploitation est synonyme de son épuisement. L'ennemi commun a changé, et l'armée s'est adaptée : c'est elle qui explore les terres environnantes à la recherche de nouveaux 
+
+gisements. Raynor Hell est l'un des leaders à la tête des expéditions dans l'inconnu.
+
+++ description des mines ?
+
+
+Le dôme
+
+À l'intérieur du dôme, une communauté vivant en autarcie s'était préparée à la catastrophe, offrant à ses habitants une solution sage et viable pour leur survie sur le long 
+
+terme. Le Dôme les protège, tandis qu'un soleil synthétique, organe vital de la structure, les alimente en énergie et leur offre un écosystème florissant que le reste du monde 
+
+croyait à jamais disparu. Le peuple est cultivé, éduqué aux pratiques respectueuses de son environnement : développement durable, intégration adéquate de la technologie dans le 
+
+quotidien et maîtrise de la consommation d'énergie définissent leur mode de vie. C'est Ed Gaïa, un vieil homme parmi d'autres, qui se charge d'initier Raynor à ce nouveau monde.
+
+
+La maison d'Ed Gaïa
+
+[images seulement ?]</p>
 	<img src="graphics/maison-EdGaia1.jpg" width="218" height="125" alt="Maison d'Ed Gaïa" class="imagecontenu" />
 	<img src="graphics/maison-EdGaia2.jpg" width="218" height="125" alt="Maison d'Ed Gaïa" class="imagecontenu" />
-	<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.</p><br />
-    
-    <p class="soustitre">Chiwonga</p>
-	<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.</p>
 
 
 </div>
