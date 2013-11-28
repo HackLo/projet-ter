2505599efb650dfd970a8ab5faf51126d0d1d4cc
Mel09 <melanie.roy09@gmail.com>
2013-11-28 16:05:33 +0100

    modif inspirations (2)

--- inspirations.php
+++ inspirations.php
@@ -20,7 +20,7 @@
 		<img src="graphics/inspiration-underthedome.jpg" class="imagecontenu" width="190" alt="Under the Dome" />
 	</div>
 	<div class="blocright">
-		<p>A Chester's Mill, les habitants se réveillent un matin coupés du monde et piégés dans la ville par un immense dôme transparent. Certains tenteront de tirer profit de cette situation inexpliquée afin de prendre le pouvoir. Mais une résistance va s'organiser  pour empêcher ces personnes de parvenir à leur fin. Ce dôme est à l'origine même de l'idée des deux univers de cette nouvelle.</p>
+		<p>Les habitants de la petite communauté de Chester's Mill se réveillent un matin coupés du monde et piégés dans la ville par un immense dôme transparent. Certains tenteront de tirer profit de cette situation inexpliquée afin de prendre le pouvoir. Mais une résistance va s'organiser  pour empêcher ces personnes de parvenir à leur fin. Ce dôme est à l'origine même de l'idée des deux univers de cette nouvelle.</p>
 	</div>
 	<div style="clear: both;"></div>
 
@@ -30,7 +30,17 @@
 		<img src="graphics/inspiration-livingtomorrow.jpg" class="imagecontenu" width="190" alt="Living Tomorrow" />
 	</div>
 	<div class="blocright">
-		<p>Living Tomorrow est un lieu de rencontres pour les entreprises novatrices. Il leur permet de faire connaître aux visiteurs des produits et services qui pourraient améliorer la qualité de vie et de travail, dans un futur proche. Ce projet a inspiré notre vision de la maison d'Ed Gaïa, notamment la maison de Vilvorde (projet de 2007 - 2012) pour l'espace intérieur et la cuisine.</p>
+		<p>Living Tomorrow est un lieu de rencontres pour les entreprises novatrices. Il leur permet de faire connaître aux visiteurs des produits et services qui pourraient améliorer la qualité de vie et de travail, dans un futur proche. Ce projet a inspiré notre vision de la maison d'Ed Gaïa, notamment la maison de Vilvorde (projet de 2007 - 2012) pour l'espace intérieur et la cuisine. Visitez la maison à travers cette <a href="http://www.youtube.com/watch?v=9DJr8QwgLEA"><u>vidéo</u></a>.</p>
+	</div>
+	<div style="clear: both;"></div>
+    
+    <div class="blocleft">
+		<h2>Philips Biosphère</h2>
+		<p>Home Farming</p>
+		<img src="graphics/inspiration-biosphere.jpg" class="imagecontenu" width="190" alt="Philips Biosphère" />
+	</div>
+	<div class="blocright">
+		<p>Ce concept de cuisine futuriste imaginée par Philips est une mini-ferme écologique et auto-suffisante, servant à produire une partie de l'alimentation d'une famille. Mettant en synergie les poissons, les légumes, les plantes et les algues, cette biosphère verticale générera ainsi de manière entièrement naturelle de la nourriture, de l'eau filtrée et du gaz domestique, en s'alimentant notamment par les déchets de la cuisine.</p>
 	</div>
 	<div style="clear: both;"></div>
 
