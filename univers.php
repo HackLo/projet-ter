e61df779d3ddea6855abebf808b02b00eb80fa72
Mel09 <melanie.roy09@gmail.com>
2013-11-28 20:03:11 +0100

    chiwongas

--- univers.php
+++ univers.php
@@ -2,37 +2,21 @@
     <h1>L'univers</h1>
 	
     <h2>La planète Terre</h2>
+	<p>Notre planète est un amoncellement de cendres sans âme dans lequel le vivant n'a plus vraiment son mot à dire. Seule espèce capable d'affronter les conséquences d'une explosion volcanique dévastatrice, l'Humain a choisi de profondément modifier son métabolisme pour amoindrir les dangers et douleurs imposés par cet environnement, qui est désormais son principal prédateur. Contraint de se prémunir pour survivre, il se retrouve esclave des technologies transhumanistes dont il rêvait autrefois et le plaisir simple de la nourriture a disparu depuis bien longtemps de son mode de vie. Le minerais d'uranium est la principale source d'énergie de ce nouveau monde, et comme toute ressource naturelle, son exploitation est synonyme de son épuisement. L'ennemi commun a changé, et l'armée s'est adaptée : c'est elle qui explore les terres environnantes à la recherche de nouveaux gisements. Raynor Hell est l'un des leaders à la tête des expéditions dans l'inconnu. ++ description des mines ?</p><br />
+
+
+	<h2>Le dôme</h2>
+	<p>À l'intérieur du dôme, une communauté vivant en autarcie s'était préparée à la catastrophe, offrant à ses habitants une solution sage et viable pour leur survie sur le long terme. Le Dôme les protège, tandis qu'un soleil synthétique, organe vital de la structure, les alimente en énergie et leur offre un écosystème florissant que le reste du monde croyait à jamais disparu. Le peuple est cultivé, éduqué aux pratiques respectueuses de son environnement : développement durable, intégration adéquate de la technologie dans le quotidien et maîtrise de la consommation d'énergie définissent leur mode de vie. C'est Ed Gaïa, un vieil homme parmi d'autres, qui se charge d'initier Raynor à ce nouveau monde.</p><br />
+
+
+	<h2>La maison d'Ed Gaïa</h2>
+	<img src="graphics/maison-EdGaia1.jpg" width="208" height="119" alt="Maison d'Ed Gaïa" class="imagecontenu" />
+	<img src="graphics/maison-EdGaia2.jpg" width="208" height="119" alt="Maison d'Ed Gaïa" class="imagecontenu" />
+	<p>[images seulement ?]</p><br />
+    
+
+	<h2>Les chiwongas</h2>
+  	<p><img src="graphics/chiwongas.jpg" width="229" height="133" alt="Chiwongas" class="imagecontenu" /> On dit du chiwonga qu'il est apparu suite à la création du soleil artificiel, subtile mélange de deux fruits appelés par les légendes poire et ananas. Malgré les apparences, un chiwonga mûr doit posséder une enveloppe à la texture plutôt souple. Sa chair est juteuse et fraichement parfumée, avec une consistance douce et crémeuse. Au goût, sa pulpe tendre et sucrée évoque la vanille. Le chiwonga est très nourrissant et doit être mangé à maturité pour assurer toute sa saveur. Ce fruit d'environ 5 cm de diamètre pousse en grappe sur des arbres et s'assèche en vieillissant.</p>
 	
-
-<p>Notre planète est un amoncellement de cendres sans âme dans lequel le vivant n'a plus vraiment son mot à dire. Seule espèce capable d'affronter les conséquences d'une explosion volcanique dévastatrice, l'Humain a choisi de profondément modifier son métabolisme pour amoindrir les dangers et douleurs imposés par cet environnement, qui est désormais son 
-
-principal prédateur. Contraint de se prémunir pour survivre, il se retrouve esclave des technologies transhumanistes dont il rêvait autrefois et le plaisir simple de la 
-
-nourriture a disparu depuis bien longtemps de son mode de vie. Le minerais d'uranium est la principale source d'énergie de ce nouveau monde, et comme toute ressource naturelle, 
-
-son exploitation est synonyme de son épuisement. L'ennemi commun a changé, et l'armée s'est adaptée : c'est elle qui explore les terres environnantes à la recherche de nouveaux 
-
-gisements. Raynor Hell est l'un des leaders à la tête des expéditions dans l'inconnu.
-
-++ description des mines ?
-
-
-Le dôme
-
-À l'intérieur du dôme, une communauté vivant en autarcie s'était préparée à la catastrophe, offrant à ses habitants une solution sage et viable pour leur survie sur le long 
-
-terme. Le Dôme les protège, tandis qu'un soleil synthétique, organe vital de la structure, les alimente en énergie et leur offre un écosystème florissant que le reste du monde 
-
-croyait à jamais disparu. Le peuple est cultivé, éduqué aux pratiques respectueuses de son environnement : développement durable, intégration adéquate de la technologie dans le 
-
-quotidien et maîtrise de la consommation d'énergie définissent leur mode de vie. C'est Ed Gaïa, un vieil homme parmi d'autres, qui se charge d'initier Raynor à ce nouveau monde.
-
-
-La maison d'Ed Gaïa
-
-[images seulement ?]</p>
-	<img src="graphics/maison-EdGaia1.jpg" width="218" height="125" alt="Maison d'Ed Gaïa" class="imagecontenu" />
-	<img src="graphics/maison-EdGaia2.jpg" width="218" height="125" alt="Maison d'Ed Gaïa" class="imagecontenu" />
-
 
 </div>
