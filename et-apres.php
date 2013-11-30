7ca36f02aa2a868b35afa19080daf5fdbe0cff34
Ewiest <ewiest@free.fr>
2013-11-30 16:37:19 +0100

    Et après, ajout des liens vers les RS
    Ajout des liens vers nos pages sur les réseaux sociaux.

--- et-apres.php
+++ et-apres.php
@@ -6,9 +6,11 @@
 	<p>Notre héros sera-t-il capable de continuer son initiation à la gastronomie ? Son errance sera-t-elle l’occasion d’une découverte encore plus incroyable ? Va-t-il un jour pouvoir retourner chez lui pour transmettre aux siens ce qu’il a appris ? C’est à toi décider !</p></br>
 
 	<h2>Comment ça marche ?</h2>
-	<p>Pour participer, rien de plus simple : rends-toi sur notre page Facebook ou Twitter et expose-nous en quelques lignes tes idées via un message sur notre mur ou un tweet.</p>
+	<p>Pour participer, rien de plus simple : rends-toi sur notre page <a href="https://www.facebook.com/CulinR">Facebook</a> ou <a href="https://twitter.com/CulinR">Twitter</a> et expose-nous en quelques lignes tes idées via un message sur notre mur ou un tweet.</p>
 	<p>Néanmoins, ton rôle sera loin d’être terminé ! Dès le 1er janvier, tu pourras voter pour ta suite préférée parmi celles qui auront été retenues par notre rédaction.</p>
 	<p>Les votes seront clos le 31 janvier. C’est alors que notre équipe d’écrivains entrera en piste pour composer ce nouvel épisode tel que vous l'aurez imaginé et élu ! Une fois postée sur le site, il ne te restera plus qu’à la déguster !</p>
 	<p>Alors si tu as envie de cuisiner le futur de Raynor Hell à ta propre sauce, ne perds plus une minute et jette-toi sur ton clavier !</p>
+	
+	<h3>Vote sur <a href="https://www.facebook.com/CulinR" class="reseauxsociaux" target="_blank"><img src="graphics/facebook.png" alt="Page Facebook Culin'R" /></a>ou <a href="https://twitter.com/CulinR" class="reseauxsociaux" target="_blank"><img src="graphics/twitter.png" alt="Page Twitter Culin'R" /></a>!</h3>
 
 </div>
