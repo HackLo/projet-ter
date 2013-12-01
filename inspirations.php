998a03cb0ff5c1a2a7b48c39fee05f597d87941b
Mel09 <melanie.roy09@gmail.com>
2013-12-01 21:18:02 +0100

    Ajout inspiration bio-dome

--- inspirations.php
+++ inspirations.php
@@ -23,6 +23,16 @@
 		<p>Les habitants de la petite communauté de Chester's Mill se réveillent un matin coupés du monde et piégés dans la ville par un immense dôme transparent. Certains tenteront de tirer profit de cette situation inexpliquée afin de prendre le pouvoir. Mais une résistance va s'organiser  pour empêcher ces personnes de parvenir à leur fin. Ce dôme est à l'origine même de l'idée des deux univers de cette nouvelle.</p>
 	</div>
 	<div style="clear: both;"></div>
+    
+    <div class="blocleft">
+		<h2>Bio-Dome</h2>
+		<p>Film de Jason Bloom</p>
+		<img src="graphics/inspiration-biodome.jpg" class="imagecontenu" width="190" alt="Bio-Dome" />
+	</div>
+	<div class="blocright">
+		<p>Deux amis se retrouvent accidentellement enfermés dans un complexe scientifique appelé "Bio-Dôme". Ce dernier, situé dans le désert de l'Arizona, consiste en une expérience écologique d'une durée d'un an. Cette comédie tourne autour d'une question : Bud et Doyle seront-ils capables de s'adapter à ce nouvel environnement ? Ce film est une autre inspiration de l'idée des deux univers et du dôme.</p>       
+	</div>
+	<div style="clear: both;"></div>
 
 	<div class="blocleft">
 		<h2>Living Tomorrow</h2>
