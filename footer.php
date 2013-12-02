fc317f863f91b57067ab3ba43294dfeacd34202d
Mel09 <melanie.roy09@gmail.com>
2013-12-02 11:16:12 +0100

    icon télécharger la nouvelle

--- footer.php
+++ footer.php
@@ -2,7 +2,8 @@
 	<div class="reseauxsociaux">
 		<a href="https://www.facebook.com/CulinR" target="_blank"><img src="graphics/facebook.png" alt="Page Facebook Culin'R" /></a>
 		<a href="https://twitter.com/CulinR" target="_blank"><img src="graphics/twitter.png" alt="Page Twitter Culin'R" /></a>
-	</div>
+        <a href="" target="_blank"><img src="graphics/pdf.png" alt="Télécharger la nouvelle" title="Télécharger la nouvelle" class="infobulle" /></a>
+</div>
 	<div class="footer">
 		<p class="copyright">© Copyright 2013 - Master Web Éditorial</p>
 		<p class="copyright"><a href="index.php?page=plan">Plan du site</a> | <a href="index.php?page=mentions">Mentions légales</a> | <a href="index.php?page=contact">Contact</a></p>
