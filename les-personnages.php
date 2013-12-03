4f8d5144db5659a6b80cf45779bbe85ee26f60ae
loicsport23 <loicsport23@gmail.com>
2013-12-03 14:16:50 +0100

    Commentaires CCS + texte personnages

--- les-personnages.php
+++ les-personnages.php
@@ -1,7 +1,14 @@
 <div id="wrapper">
   <h1>Les personnages</h1>
-	<img src="graphics/RaynorHell.jpg" width="447" height="332" alt="Raynor Hell" class="imagecontenu" />
-    <p>Piste audio</p>
-    <img src="graphics/EdGaia.jpg" width="447" height="332" alt="Ed Gaïa" class="imagecontenu" />
-    <p>Piste audio</p>
+	<div>
+		<img src="graphics/RaynorHell.jpg" width="447" height="332" alt="Raynor Hell" class="imagecontenu" />
+	    <p>Piste audio</p>
+	    <p>Je naquis il y a trente ans au sein d'une colonie dans un monde dévasté par l'éruption du grand volcan. La femme qui me donna la vie succomba à l'accouchement et mon géniteur, extracteur de minerais, périt dans les flammes peu avant ma naissance. C'est ainsi que les ouvrières en charge de ma survie me donnèrent le nom évocateur de Raynor Hell. Mon quotidien et celui de mes pairs est un enfer, mais je m'échine à le rendre plus confortable en recherchant de nouvelles sources d'énergie. A l'orée de l'extinction de mon peuple, je fus le premier à découvrir le dôme qui nous redonnera espoir et prospérité. Du moins je le croyais...</p>
+	</div>
+	<div>
+		<img src="graphics/EdGaia.jpg" width="447" height="332" alt="Ed Gaïa" class="imagecontenu" />
+    	<p>Piste audio</p>
+    	<p>Depuis des dizaines de générations, ma famille s'est illustrée dans la recherche et les sciences. Mes ancêtres furent à l'initiative de la construction du dôme en qualité de biologistes spécialisés dans les écosystèmes artificiels. Naturellement, j'ai suivi la vocation familiale en choisissant le credo de l'agronomie. Ce métier est le trait d'union entre mes deux passions, l'agriculture et la cuisine. S'il m'est aisé de produire et de cuisiner de sains et goûteux produits, je déplore un manque d'adresse pour les relations sociales. A l'instar de mes parents et des leurs avant eux, j'aurais aimé transmettre mes connaissances à une jeune oreille attentive. Trop âgé, je ne suis plus en mesure de procréer mais je ne perds l'espoir de trouver un disciple avant mon dernier souffle.</p>
+	</div>
+    
 </div>
