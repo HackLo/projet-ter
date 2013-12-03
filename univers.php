d120716dff788ce7d0aa61aef5165d65299cd848
lucalerou <lucalerou@gmail.com>
2013-12-03 08:42:56 +0100

    univers (à modif)

--- univers.php
+++ univers.php
@@ -2,7 +2,9 @@
     <h1>L'univers</h1>
 	
     <h2>La planète Terre</h2>
-	<p>Notre planète est un amoncellement de cendres sans âme dans lequel le vivant n'a plus vraiment son mot à dire. Seule espèce capable d'affronter les conséquences d'une explosion volcanique dévastatrice, l'Humain a choisi de profondément modifier son métabolisme pour amoindrir les dangers et douleurs imposés par cet environnement, qui est désormais son principal prédateur. Contraint de se prémunir pour survivre, il se retrouve esclave des technologies transhumanistes dont il rêvait autrefois et le plaisir simple de la nourriture a disparu depuis bien longtemps de son mode de vie. Le minerais d'uranium est la principale source d'énergie de ce nouveau monde, et comme toute ressource naturelle, son exploitation est synonyme de son épuisement. L'ennemi commun a changé, et l'armée s'est adaptée : c'est elle qui explore les terres environnantes à la recherche de nouveaux gisements. Raynor Hell est l'un des leaders à la tête des expéditions dans l'inconnu. ++ description des mines ?</p><br />
+    <img src="graphics/monde1.jpg" alt="Monde de Raynor Hell" class="imagecontenu" />
+	<img src="graphics/monde2.jpg" alt="Monde de Raynor Hell" class="imagecontenu" />
+	<p>Notre planète est un amoncellement de cendres sans âme dans lequel le vivant n'a plus vraiment son mot à dire. Seule espèce capable d'affronter les conséquences d'une explosion volcanique dévastatrice, l'Homme a choisi de profondément modifier son métabolisme pour amoindrir les dangers et douleurs imposés par cet environnement, qui est désormais son principal prédateur. Contraint de se prémunir pour survivre, il se retrouve esclave des technologies transhumanistes dont il rêvait autrefois et le plaisir simple de la nourriture a disparu depuis bien longtemps de son mode de vie. Le minerais d'uranium est la principale source d'énergie de ce nouveau monde, et comme toute ressource naturelle, son exploitation est synonyme de son épuisement. Une pénurie potentielle des ressources constitue désormais l'ennemi commun, et l'armée s'est adaptée : c'est elle qui explore les terres environnantes à la recherche de nouveaux gisements. Raynor Hell est l'un des leaders à la tête des expéditions dans les terres inconnues.</p><br />
 
 
 	<h2>Le dôme</h2>
@@ -10,13 +12,14 @@
 
 
 	<h2>La maison d'Ed Gaïa</h2>
-	<img src="graphics/maison-EdGaia1.jpg" width="208" height="119" alt="Maison d'Ed Gaïa" class="imagecontenu" />
+    <img src="graphics/maison-EdGaia1.jpg" width="208" height="119" alt="Maison d'Ed Gaïa" class="imagecontenu" />
 	<img src="graphics/maison-EdGaia2.jpg" width="208" height="119" alt="Maison d'Ed Gaïa" class="imagecontenu" />
-	<p>[images seulement ?]</p><br />
-    
+    <p>Acquisition récompensant une longue vie de labeur, la maison d'Ed Gaïa est le parfait exemple des habitats ultra-modernes qui se développent sous le dôme. Ed passe l'essentiel de son temps dans la cuisine, qui fait manifestement l'objet de toutes ses attentions. L'équipement de cette pièce démontre la capacité des habitants du dôme à développer des solutions à leurs besoins les plus hédonistes tout en conservant de profonds savoir-faire : un plan de travail déformable à souhait lui permet d'exécuter n'importe quelle recette et divers légumes, fruits, épices, viandes et poissons remplissent les rangements, formant une source infinie d'inspiration pour cet amoureux de la gastronomie. Les autres pièces ne sont pas en reste, les objets qu'elles contiennent ne manqueront pas de vous étonner.</p>
+
 
 	<h2>Les chiwongas</h2>
-  	<p><img src="graphics/chiwongas.jpg" width="229" height="133" alt="Chiwongas" class="imagecontenu" /> On dit du chiwonga qu'il est apparu suite à la création du soleil artificiel, subtile mélange de deux fruits appelés par les légendes poire et ananas. Malgré les apparences, un chiwonga mûr doit posséder une enveloppe à la texture plutôt souple. Sa chair est juteuse et fraichement parfumée, avec une consistance douce et crémeuse. Au goût, sa pulpe tendre et légèrement sucrée évoque la pastèque. Le chiwonga est très nourrissant et doit être mangé à maturité pour assurer toute sa saveur. Ce fruit d'environ 5 cm de diamètre pousse en grappe sur des arbres et s'assèche en vieillissant.</p>
+  	<img src="graphics/chiwongas.jpg" width="229" height="133" alt="Chiwongas" class="imagecontenu" />
+    <p>On dit du chiwonga qu'il est apparu suite à la création du soleil artificiel, subtile mélange de deux fruits appelés par les légendes poire et ananas. Malgré les apparences, un chiwonga mûr doit posséder une enveloppe à la texture plutôt souple. Sa chair est juteuse et fraichement parfumée, avec une consistance douce et crémeuse. Au goût, sa pulpe tendre et légèrement sucrée évoque la pastèque. Le chiwonga est très nourrissant et doit être mangé à maturité pour assurer toute sa saveur. Ce fruit d'environ 5 cm de diamètre pousse en grappe sur des arbres et s'assèche en vieillissant.</p>
 	
 
 </div>
