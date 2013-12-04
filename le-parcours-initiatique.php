c7897de9d4d313b195cf382c3adddcd4fb7be642
Ewiest <ewiest@free.fr>
2013-12-04 10:17:53 +0100

    Parcours initiatique
    Modification du livre

--- le-parcours-initiatique.php
+++ le-parcours-initiatique.php
@@ -15,7 +15,7 @@
 	<p>Prenez deux mondes avec une culture différente.<br />
         Ajoutez-y un infiltré.<br />
         Faites mariner un soupçon d'étonnement et de réticence face à un petit déjeuner.<br />
-        Ensuite, incorporez à l'incompréhension une pincée d'attirance à la vue d'un livre de cuisine.<br />
+        Ensuite, incorporez à l'incompréhension une pincée d'attirance à la vue d'une recette projetée par un holographe.<br />
         Préchauffez votre appétit et goûtez enfin à la préparation.<br />
         Une fois l'intérêt grandissant, faites fondre les craintes et enfournez la curiosité. Simultanément, assaisonnez la préparation avec de nouveaux produits.<br />
         Dans un saladier, versez la réalisation et saupoudrez de curiosité avec l'aide d'un ami.<br />
