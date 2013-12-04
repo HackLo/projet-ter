6cd8b3fbf5af15c8e5689b5a63f476a0b5bbecde
Mel09 <melanie.roy09@gmail.com>
2013-12-04 12:59:46 +0100

    audio personnages

--- les-personnages.php
+++ les-personnages.php
@@ -4,18 +4,30 @@
   <h1>Les personnages</h1>
 	<div>
 		<img src="graphics/RaynorHell.jpg" width="447" height="332" alt="Raynor Hell" class="imagecontenu" />
-	    <p>Piste audio</p>
-        <a href="javascript:void(look('Text1Depliant'));" id="a">Raynor Hell</a><br />
+        <audio controls>
+			<source src="documents/RaynorHell.mp3" type="audio/mp3" />
+			<object type="application/x-shockwave-flash" data="documents/dewplayer/dewplayer.swf" width="200" height="20" id="dewplayer" name="dewplayer">
+				<param name="movie" value="documents/dewplayer/dewplayer.swf" />
+				<param name="flashvars" value="mp3=documents/RaynorHell.mp3" />
+			</object>
+		</audio><br /><br />
+        Voir le discours de Raynor Hell <a href="javascript:void(look('Text1Depliant'));" id="a"><img src="graphics/plus.png" alt="+" /></a><br />
         <div id="Text1Depliant" style="display:none">
 			<p>Je naquis il y a trente ans au sein d'une colonie dans un monde dévasté par l'éruption du grand volcan. La femme qui me donna la vie succomba à l'accouchement et mon géniteur, extracteur de minerais, périt dans les flammes peu avant ma naissance. C'est ainsi que les ouvrières en charge de ma survie me donnèrent le nom évocateur de Raynor Hell. Mon quotidien et celui de mes pairs est un enfer, mais je m'échine à le rendre plus confortable en recherchant de nouvelles sources d'énergie. A l'orée de l'extinction de mon peuple, je fus le premier à découvrir le dôme qui nous redonnera espoir et prospérité. Du moins je le croyais...</p>
         </div>
-	</div>
+	</div><br /><br />
     
 	<div>
 		<img src="graphics/EdGaia.jpg" width="447" height="332" alt="Ed Gaïa" class="imagecontenu" />
-    	<p>Piste audio</p>
-        <a href="javascript:void(look('Text2Depliant'));" id="a">Ed Gaia</a><br />
-        <div id="Text2Depliant" style="display:none">
+    	<audio controls>
+		  <source src="documents/EdGaia.mp3" type="audio/mp3" />
+			<object type="application/x-shockwave-flash" data="documents/dewplayer/dewplayer.swf" width="200" height="20" id="dewplayer" name="dewplayer">
+				<param name="movie" value="documents/dewplayer/dewplayer.swf" />
+			  <param name="flashvars" value="mp3=documents/EdGaia.mp3" />
+			</object>
+		</audio><br /><br />
+        Voir le discours d'Ed Gaia <a href="javascript:void(look('Text2Depliant'));" id="a"><img src="graphics/plus.png" alt="+" /></a><br />
+  <div id="Text2Depliant" style="display:none">
 			<p>Depuis des dizaines de générations, ma famille s'est illustrée dans la recherche et les sciences. Mes ancêtres furent à l'initiative de la construction du dôme en qualité de biologistes spécialisés dans les écosystèmes artificiels. Naturellement, j'ai suivi la vocation familiale en choisissant le credo de l'agronomie. Ce métier est le trait d'union entre mes deux passions, l'agriculture et la cuisine. S'il m'est aisé de produire et de cuisiner de sains et goûteux produits, je déplore un manque d'adresse pour les relations sociales. A l'instar de mes parents et des leurs avant eux, j'aurais aimé transmettre mes connaissances à une jeune oreille attentive. Trop âgé, je ne suis plus en mesure de procréer mais je ne perds l'espoir de trouver un disciple avant mon dernier souffle.</p>
         </div>     
 	</div>
